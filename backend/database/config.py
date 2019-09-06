bf8e89c35aebec0e8938fee7178dc2e5bf7a320f
Prashant Varshney <pv03158@gmail.com>
2019-09-07 01:10:27 +0530

    structured a folder named database for db queries

---
+++ backend/database/config.py
@@ -0,0 +1,5 @@
+# This file contains all the configurations essential to connect to database and server
+# Use this file to store keys of database, server, etc.
+# And to apply that details import this file into your program as a module
+
+MongoDB_URI = "mongodb+srv://prashant:varshney@cluster0-jv09r.mongodb.net/test?retryWrites=true&w=majority"
