cffb676ae80ace64d5c9871fde1af71818a13315
Prashant Varshney <pv03158@gmail.com>
2019-09-08 06:04:21 +0530

    added faculty.query and faculty.remove method

--- backend/database/DBQuery.py
+++ backend/database/DBQuery.py
@@ -1,6 +1,7 @@
 # This module acts as an API for CRUD operations in database
 from pymongo import MongoClient
 import config
+import datetime
 
 # Creating connection with the mongodb database
 client = MongoClient(config.MongoDB_URI)        # Use this client object to query database
@@ -14,13 +15,14 @@ db = client[config.Database_Name]               # Use this db object to query co
 ## 4. Update faculty profile with method name - update
 
 class faculty:
-	def insert(self,id,name,phone_numbers,email,subjects,qualifications=[],time_table={},classes=[],ratings=5,reviews=[]):
+	def insert(self,id,name,dob,phone_numbers,email,subjects,qualifications=[],time_table={},classes=[],ratings=5,reviews=[]):
 		# This insert method inputs necessary details of faculty through input parameters and then
 	    # insert it into database if it doesn't presents in DB
 	    #
 	    # Datastructure of input parameters :
-	    # id --> integer
-	    # name --> string
+	    # id --> string
+	    # name --> dictionary
+	    # dob --> dictionary
 	    # phone numbers --> list of integer value
 	    # email --> list of string
 	    # subjects --> list of string
@@ -35,11 +37,12 @@ class faculty:
 	    # 	 provided default values are used
 		# 	*id is the primary key for faculty profile collections
 		# 	*collection name that is going to be used : faculty_profile
-
+		
 	    # Creating dictionary of the document that is required to insert in DB
 	    document = {
 			    "faculty_id":id,
 			    "name":name,
+			    "date_of_birth":dob,
 			    "phone_numbers":phone_numbers,
 			    "email":email,
 			    "subjects":subjects,
@@ -49,7 +52,28 @@ class faculty:
 			    "ratings":ratings,
 			    "reviews":reviews
 	    		}
-	    status = db.faculty_profile.insert_one(document)
-	################### End of insert method ####################
-	
+	    duplicate_entry = db[config.Faculty_Profile_Collection].find_one({ f'faculty_id':f'{id}' })
+	    print(duplicate_entry)
+	    if duplicate_entry != None:
+	    	print('[ Error ] Object of this ID already present in database')
+	    	return False
 
+	    status = db[config.Faculty_Profile_Collection].insert_one(document)
+	    print(f'[ INFO  ] {status}') 	# Printing Status of result of query
+	    return True
+	################### End of insert method ####################
+
+	def query(self,query_parameter,query_value):
+		# This query function inputs query parameter like faculty_id, name, etc. and query value
+		# to search documents in collection. After successful search it returns 
+		# rest of the details to the user.
+		return list(db[config.Faculty_Profile_Collection].find({ f'{query_parameter}':f'{query_value}' }))
+	################### End of query_value method ####################		
+
+	def remove(self,query_parameter,query_value):
+		# This remove function inputs query parameter like faculty_id, name, etc. and query value
+		# to search documents in collection. After that remove them from collection
+		
+fac = faculty()
+x = fac.insert('055','somya',['97384827186'],['somyji@gmail.com'],['History'])
+print(x)
