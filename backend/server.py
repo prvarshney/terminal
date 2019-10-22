7c31eac08472507b53d593bbcfef460948f097d3
Prashant Varshney <pv03158@gmail.com>
2019-10-22 12:22:02 +0530

    basic server arch

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,17 @@
 from database import DBQuery as db
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/",methods=['POST'])
+def authentication():
+    user_credentials = request.get_json()
+    print(user_credentials['user_id'])
+    print(user_credentials['password'])
+    response = { 'status':'300' }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5001,host="0.0.0.0")
