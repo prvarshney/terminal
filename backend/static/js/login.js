0e519cf5f85c65452f568a03eb550f17e182efa4
Preeti <preeti09011999@gmail.com>
2020-01-15 01:35:25 +0530

    Delete-One

--- backend/static/js/login.js
+++ backend/static/js/login.js
@@ -25,7 +25,7 @@ submitBtn.addEventListener('click', ()=>{
         loader.style.display = 'none';
         if( response.status == 200 ){    // EXECUTES WHEN LOGIN GETS SUCCESSFUL
             // REDIRECTING TO DASHBOARD
-            window.location.href = "/admin/dashboard";
+            window.location.href = "/admin/queryTable";
         }
         else{       // EXECUTES WHEN LOGIN WAS UNSUCCESSFUL
             console.log(response);
