56f1f5f7140e66763bde2470961e871cb2eff9f1
Prashant Varshney <pv03158@gmail.com>
2020-01-15 15:52:18 +0530

    quick fix of deleteOne button

--- backend/static/js/show-all.js
+++ backend/static/js/show-all.js
@@ -1,5 +1,32 @@
-let show_all = document.getElementById('queryBtn');
+let removeBtns = document.getElementsByClassName('remove-enrollments-btn');
+let batchName = document.getElementById('batch-name').innerHTML.split('_');
+let programme = batchName[0];
+let branch = batchName[1];
+let section = batchName[2];
+let year_of_pass = batchName[3];
 
-show_all.addEventListener('click', ()=>{
-    window.location.href = "/showAll";
-});
+// EVNET LISTENERS ---START
+for(let itr = 0; itr < removeBtns.length ; itr++){
+    removeBtns[itr].addEventListener('click', () => {
+        enrollment = removeBtns[itr].getAttribute('name');
+        jsonBody = {
+            'programme':programme,
+            'branch':branch,
+            'section':section,
+            'year_of_pass':year_of_pass,
+            'enrollment':[ enrollment ]
+        }
+        fetch('/admin/remove', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+            credentials: 'same-origin',
+            body: JSON.stringify( jsonBody )
+        }).then( response => response.json())
+        .then( response => {
+            if( response['status'][enrollment] == 220 ){
+                removeBtns[itr].parentElement.parentElement.style.display = 'none';
+            }
+        } )
+    })
+}
+// EVNET LISTENERS ---END
