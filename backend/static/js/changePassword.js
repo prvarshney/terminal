0e25403f884595cb0b19d9860b776df8dbf9ab9f
Somya <somya1234@gmail.com>
2020-01-19 12:20:58 +0530

    added eyeToggler to forgotPassword and ChangePassword

--- backend/static/js/changePassword.js
+++ backend/static/js/changePassword.js
@@ -3,8 +3,57 @@ let newPassword = document.getElementById('newPassword');
 let confirmPassword = document.getElementById('confirmPassword');
 let submitBtn = document.getElementById('submitBtn');
 let loader = document.getElementById('loader');
+let eyeTogglerCurrent = document.getElementById('eyeTogglerCurrent');
+let eyeTogglerNew = document.getElementById('eyeTogglerNew');
+let eyeTogglerConfirm = document.getElementById('eyeTogglerConfirm');
+let eyeTogglerStatus = false;
 
 // EVENT LISTENERS --START
+eyeTogglerCurrent.addEventListener('click', ()=>{
+    if(!eyeTogglerStatus){
+        eyeTogglerStatus = true;
+        eyeTogglerCurrent.classList.remove('fa-eye-slash');
+        eyeTogglerCurrent.classList.add('fa-eye');
+        currentPassword.type = 'text';
+    }
+    else{
+        eyeTogglerStatus = false;
+        eyeTogglerCurrent.classList.remove('fa-eye');
+        eyeTogglerCurrent.classList.add('fa-eye-slash');
+        currentPassword.type = 'password';
+    }
+});
+
+eyeTogglerNew.addEventListener('click', ()=>{
+    if(!eyeTogglerStatus){
+        eyeTogglerStatus = true;
+        eyeTogglerNew.classList.remove('fa-eye-slash');
+        eyeTogglerNew.classList.add('fa-eye');
+        newPassword.type = 'text';
+    }
+    else{
+        eyeTogglerStatus = false;
+        eyeTogglerNew.classList.remove('fa-eye');
+        eyeTogglerNew.classList.add('fa-eye-slash');
+        newPassword.type = 'password';
+    }
+});
+
+eyeTogglerConfirm.addEventListener('click', ()=>{
+    if(!eyeTogglerStatus){
+        eyeTogglerStatus = true;
+        eyeTogglerConfirm.classList.remove('fa-eye-slash');
+        eyeTogglerConfirm.classList.add('fa-eye');
+        confirmPassword.type = "text";
+    }
+    else{
+        eyeTogglerStatus = false;
+        eyeTogglerConfirm.classList.remove('fa-eye');
+        eyeTogglerConfirm.classList.add('fa-eye-slash');
+        confirmPassword.type = "password";
+    }
+})
+
 submitBtn.addEventListener('click', ()=>{
     // LOADING LOADER.GIF
     loader.style.display = "block";
