161c787001056fda22734fdce406fea5e478b578
Prashant Varshney <pv03158@gmail.com>
2020-02-12 01:39:34 +0530

    created sign up activity with selected color palletes

--- frontend/app/src/main/java/com/thethreemusketeers/terminal/MainActivity.java
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/MainActivity.java
@@ -1,14 +1,56 @@
 package com.thethreemusketeers.terminal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView termsAndConditionLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        termsAndConditionLink = findViewById(R.id.tc_link);
+
+        // creating termsAndConditionLink clickable
+        SpannableString termsAndCondition = new SpannableString("Tap \"Agree and Continue\" to accept the Terms of Service and Privacy Policy.");
+        ClickableSpan termsAndConditionActivityLauncher = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                // launch new activity
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                ds.setUnderlineText(false);
+                ds.setColor( getResources().getColor(R.color.colorMatteGreen));
+                ds.setFakeBoldText(true);
+            }
+        };
+        ClickableSpan privacyPolicyActivityLauncher = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                // start new activity
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                ds.setColor(getResources().getColor(R.color.colorMatteGreen));
+                ds.setUnderlineText(false);
+            }
+        };
+        termsAndCondition.setSpan(termsAndConditionActivityLauncher,39,55, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        termsAndCondition.setSpan(privacyPolicyActivityLauncher,60,74, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        termsAndConditionLink.setText(termsAndCondition);
     }
 }
