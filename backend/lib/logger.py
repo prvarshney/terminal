acc6ed50905587cabaa60401d6cae271dbe67967
Prashant Varshney <pv03158@gmail.com>
2020-03-08 19:18:42 +0530

    changes logger to print on console only

--- backend/lib/logger.py
+++ backend/lib/logger.py
@@ -3,8 +3,9 @@ from datetime import datetime
 def log(info_msg):
     date = datetime.now()
     date_string = date.strftime('[ %d-%m-%Y :: %H:%M:%S ] ')
-    with open('DBQuery.log','a') as fd:
-        fd.write(date_string+info_msg+'\n')
+    # with open('DBQuery.log','a') as fd:
+    #     fd.write(date_string+info_msg+'\n')
+    print((date_string+info_msg)
 
 if __name__ == "__main__":
     pass
