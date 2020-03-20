a4740e2ef4bcd741f502c6efe331519f33bebc3e
Prashant Varshney <pv03158@gmail.com>
2020-03-20 14:37:47 +0530

    fix bug in studentforgotpassword

---
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/StudentRecoverPassword.java
@@ -0,0 +1,16 @@
+package com.thethreemusketeers.terminal.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thethreemusketeers.terminal.R;
+
+public class StudentRecoverPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_recover_password);
+    }
+}
