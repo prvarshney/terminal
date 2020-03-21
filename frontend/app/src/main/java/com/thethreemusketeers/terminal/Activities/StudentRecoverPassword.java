44dbe5b8ae28e79ab33ebd9a059c33bb11a50aac
Somya <somya1234@gmail.com>
2020-03-21 15:09:51 +0530

    kabada maching

--- frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/StudentRecoverPassword.java
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/StudentRecoverPassword.java
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.thethreemusketeers.terminal.Config;
+import com.thethreemusketeers.terminal.JSONResponseObject.MessageStatusEmailResponse;
 import com.thethreemusketeers.terminal.R;
 
-public class StudentRecoverPassword extends AppCompatActivity {
+public class StudentRecoverPassword<val> extends AppCompatActivity {
 
     EditText username;
-    TextView attentionRequiredTowardsUsernameField;
-    Button loginBtn;
-    Boolean proceedingFlag = true;
+    TextView attentionRequiredTowardsUsernameField, attentionRequiredTowardsInvalid;
+    Button nextBtn;
+    Boolean proceedingFlag = false, invalidAttemptFlag= false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,27 +35,66 @@ public class StudentRecoverPassword extends AppCompatActivity {
 
         //Fetching XML Elements
         username = findViewById(R.id.username);
-        loginBtn = findViewById(R.id.login_btn);
+        nextBtn = findViewById(R.id.next_btn);
         attentionRequiredTowardsUsernameField = findViewById(R.id.attention_required_on_username_editText);
+        attentionRequiredTowardsInvalid = findViewById(R.id.attention_required_on_username_editText);
 
         //CHECK THE CREDENTIALS ENTERED BY THE USER
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
         final String ReqURL = Config.HostURL + "/student/forgot_password/<user_id>";
 
-        loginBtn.setOnClickListener((new View.OnClickListener() {
+        nextBtn.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String usernameValue = username.getText().toString();
 
-                if(usernameValue.equals("")){
-                    attentionRequiredTowardsUsernameField.setAlpha(1);
-                    proceedingFlag = false;
+                if(!usernameValue.equals("")){
+                     //SENDING POST REQ TO THE SERVER TO CHECK WHETHER USER SELECTED PASSWORD
+                    // EXISTS OR NOT
+
+                    val stringRequest = new StringRequest(
+                            Request.Method.GET,
+                            ReqURL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Gson gson = new Gson();
+                                    MessageStatusEmailResponse res = gson.fromJson(response.toString(), MessageStatusEmailResponse.class);
+                                    if(res.status == 206){
+                                        invalidAttemptFlag = true;
+                                        attentionRequiredTowardsInvalid.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                                        attentionRequiredTowardsInvalid.setText("*Invalid Enrollment, Check Again");
+                                        attentionRequiredTowardsInvalid.setAlpha(1);
+                                        proceedingFlag = false;
+                                    } else if (res.status == 200) {
+                                        attentionRequiredTowardsInvalid.setAlpha(0);
+                                        startActivity(new Intent(StudentRecoverPassword.this, CreateProfile1.class));
+
+                                    }
+                                }
+                            },
+                            new Response.ErrorListener(){
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("Response %s".format(error.toString()));
+//                                    textView.text = "ERROR: %s".format(error.toString())
+
+                                }
+                            }
+                    );
+                    requestQueue.add((Request<Object>) stringRequest);
+
                 } else {
-                    attentionRequiredTowardsUsernameField.setAlpha(0);
-                    proceedingFlag = true;
-                }
-                if(proceedingFlag){
-                    startActivity(new Intent(StudentRecoverPassword.this, MainActivity.class));
+
+                    if (usernameValue.equals("")) {
+                        attentionRequiredTowardsUsernameField.setAlpha(1);
+                        attentionRequiredTowardsUsernameField.setText("*Required");
+                        proceedingFlag = false;
+                    } else {
+                        attentionRequiredTowardsUsernameField.setAlpha(0);
+                        proceedingFlag = true;
+                    }
                 }
             }
         }));
--- frontend/app/src/main/java/com/thethreemusketeers/terminal/JSONResponseObject/MessageStatusEmailResponse.java
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/JSONResponseObject/MessageStatusEmailResponse.java
@@ -1,7 +1,7 @@
 package com.thethreemusketeers.terminal.JSONResponseObject;
 
-public class MessageAndStatusResponse {
+public class MessageStatusEmailResponse {
     public String msg;
     public int status;
-    public String email_ids;
+
 }
