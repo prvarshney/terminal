9a26f3e322a3904ebcbc1bcf3ed205f13f49df4b
Somya <somya1234@gmail.com>
2020-03-22 14:18:54 +0530

    xml file including

---
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/StudentRecoverPassword1.java
@@ -0,0 +1,19 @@
+package com.thethreemusketeers.terminal.Activities;
+
+import android.os.Bundle;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.thethreemusketeers.terminal.R;
+import org.json.JSONObject;
+
+
+public class StudentRecoverPassword1 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_recover_password1);
+    }
+}
