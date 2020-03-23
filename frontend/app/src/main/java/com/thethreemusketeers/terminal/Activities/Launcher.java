2cf68610392cf46df14323fbf9bd3b80a007cbed
Prashant Varshney <pv03158@gmail.com>
2020-03-23 14:03:37 +0530

    created keys for sharedPrefrences file

--- frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/Launcher.java
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/Launcher.java
@@ -1,8 +1,14 @@
-package com.thethreemusketeers.terminal;
+package com.thethreemusketeers.terminal.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.thethreemusketeers.terminal.R;
 
 public class Launcher extends AppCompatActivity {
 
