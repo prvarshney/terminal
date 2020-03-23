54d593b633a63e1a5c4e018ee58d227ef91a76b0
Prashant Varshney <pv03158@gmail.com>
2020-03-23 16:02:14 +0530

    check sharedPref for logged in status

--- frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/Launcher.java
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/Launcher.java
@@ -16,5 +16,22 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        // CHECKING WHETHER USER IS ALREADY LOGGEDIN OR NOT
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.user_credentials_file_key),Context.MODE_PRIVATE);
+        boolean isLoggedIn = sharedPref.getBoolean(getString(R.string.login_status),false);
+        // IF USER IS NOT LOGGED IN ROUTING HIM/HER TO MAIN_ACTIVITTY AFTER 2000MS
+        if ( !isLoggedIn ) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(Launcher.this, MainActivity.class));
+                }
+            },2000);
+        }
+        else {
+            // CONNECTING WITH SERVER TO FETCH DASHBOARD
+        }
     }
 }
