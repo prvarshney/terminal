d73c66383e67729e49621159e6d9e72c69c3aa01
Prashant Varshney <pv03158@gmail.com>
2020-03-24 23:30:07 +0530

    created identity proof picker activity

---
+++ frontend/app/src/main/java/com/thethreemusketeers/terminal/Activities/UploadIdentityProof.java
@@ -0,0 +1,53 @@
+package com.thethreemusketeers.terminal.Activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.thethreemusketeers.terminal.R;
+
+public class UploadIdentityProof extends AppCompatActivity {
+
+    ImageView uploadBtn, deleteBtn, previewImageView;
+    final int GALLERY_REQUEST_CODE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_identity_proof);
+
+        // FETCHING ELEMENTS FROM XML DESIGN
+        uploadBtn = findViewById(R.id.image_upload_btn);
+        deleteBtn = findViewById(R.id.image_remove_btn);
+        previewImageView = findViewById(R.id.attach_avatar);
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( Intent.ACTION_PICK ,MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+                startActivityForResult(intent,GALLERY_REQUEST_CODE);
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previewImageView.setImageResource( R.drawable.idcard_template );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK ) {
+            previewImageView.setImageURI(data.getData());
+        }
+    }
+}
